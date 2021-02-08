c68f73cadae3a1c49fe6d97a4c6974167122dddb
Noah Harris <mail@gastrodon.io>
2021-02-07 23:51:50 -0500

    Make requests to known host
    Since the front and backend are divorced and in separate containers,
    requests should be made to a known host instead of the localhost

--- static/app.js
+++ static/app.js
@@ -3,7 +3,7 @@ const Controller = {
     ev.preventDefault();
     const form = document.getElementById("form");
     const data = Object.fromEntries(new FormData(form));
-    const response = fetch(`/search?q=${data.query}`).then((response) => {
+    const response = fetch(`http://search:3001/search?q=${data.query}`).then((response) => {
       response.json().then((results) => {
         Controller.updateTable(results);
       });
