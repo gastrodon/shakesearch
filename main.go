b7dcacd1579c2c1337f20d5fdadff6a73bc7c160
Noah Harris <mail@gastrodon.io>
2021-02-08 02:00:52 -0500

    Move searcher
    Move Searcher into its own file

--- main.go
+++ main.go
@@ -3,34 +3,12 @@ package main
 import (
 	"github.com/gastrodon/groudon/v2"
 
-	"index/suffixarray"
-	"io/ioutil"
 	"net/http"
 )
 
 const (
 	ROUTE_SEARCH = "^/search/?$"
 )
-
-func max(this, that int) (it int) {
-	if this < that {
-		it = that
-		return
-	}
-
-	it = this
-	return
-}
-
-func min(this, that int) (it int) {
-	if this > that {
-		it = that
-		return
-	}
-
-	it = this
-	return
-}
 
 func main() {
 	var err error
@@ -42,12 +20,6 @@ func main() {
 	groudon.AddHandler("GET", ROUTE_SEARCH, handleSearch(searcher))
 	http.HandleFunc("/", groudon.Route)
 	http.ListenAndServe(":3001", nil)
-}
-
-type Searcher struct {
-	CompleteWorks string
-	SuffixArray   *suffixarray.Index
-	size          int
 }
 
 func handleSearch(searcher Searcher) func(*http.Request) (int, map[string]interface{}, error) {
@@ -65,30 +37,4 @@ func handleSearch(searcher Searcher) func(*http.Request) (int, map[string]interf
 		}
 		return
 	}
-}
-
-func (search *Searcher) Load(file string) (err error) {
-	var data []byte
-	if data, err = ioutil.ReadFile(file); err != nil {
-		return
-	}
-
-	search.CompleteWorks = string(data)
-	search.SuffixArray = suffixarray.New(data)
-	search.size = len(data)
-	return
-}
-
-func (search *Searcher) Search(query string) (results []string) {
-	var indexes []int = search.SuffixArray.Lookup([]byte(query), -1)
-	results = make([]string, len(indexes))
-
-	var index, head, tail int
-	for index = range indexes {
-		head = max(0, indexes[index]-250)
-		tail = min(search.size, indexes[index]+250)
-		results[index] = search.CompleteWorks[head:tail]
-	}
-
-	return
 }
---
+++ searcher.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"index/suffixarray"
+	"io/ioutil"
+)
+
+func max(this, that int) (it int) {
+	if this < that {
+		it = that
+		return
+	}
+
+	it = this
+	return
+}
+
+func min(this, that int) (it int) {
+	if this > that {
+		it = that
+		return
+	}
+
+	it = this
+	return
+}
+
+type Searcher struct {
+	CompleteWorks string
+	SuffixArray   *suffixarray.Index
+	size          int
+}
+
+func (search *Searcher) Load(file string) (err error) {
+	var data []byte
+	if data, err = ioutil.ReadFile(file); err != nil {
+		return
+	}
+
+	search.CompleteWorks = string(data)
+	search.SuffixArray = suffixarray.New(data)
+	search.size = len(data)
+	return
+}
+
+func (search *Searcher) Search(query string) (results []string) {
+	var indexes []int = search.SuffixArray.Lookup([]byte(query), -1)
+	results = make([]string, len(indexes))
+
+	var index, head, tail int
+	for index = range indexes {
+		head = max(0, indexes[index]-250)
+		tail = min(search.size, indexes[index]+250)
+		results[index] = search.CompleteWorks[head:tail]
+	}
+
+	return
+}
